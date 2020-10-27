a57a44e3581375096c740262ed285a4b5ca02422
McLean Carpenter <carpemc@gmail.com>
2020-10-27 12:24:22 -0600

    implemented es6 class properties

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 export default class AddOption extends React.Component {
+    state = {
+      error: undefined
+    };
+  
     constructor(props) {
       super(props);
       this.handleAddOption = this.handleAddOption.bind(this);
-      this.state = {
-        error: undefined
-      };
     }
-    handleAddOption(e) {
+    handleAddOption = (e) => {
       e.preventDefault();
   
       const option = e.target.elements.option.value.trim();
@@ -19,7 +20,7 @@ export default class AddOption extends React.Component {
       if (!error) {
         e.target.elements.option.value = '';
       }
-    }
+    };
     render() {
       return (
         <div>
